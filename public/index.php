edf0f870e9108cdf217b3d606f2a7c99216f216a
Chris Hilsdon <chris@koolserve.uk>
2017-06-13 00:17:28 +0100

    Initial commit :rocket:

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->get('/', function () use ($app) {
+    return $app->json(true);
+});
+
+$app->run();
